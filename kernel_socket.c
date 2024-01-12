0b417439a72a9f2d38d640132b2a8221ebc3b07c
FenrirRhogar <103751641+FenrirRhogar@users.noreply.github.com>
2024-01-12 16:44:30 +0200

    socket.h created
    also kapoia pragmata apo to socket.c etoima (kai dior8wsh ena or8ografiko sto tinyos.h giati etsi)

--- kernel_socket.c
+++ kernel_socket.c
@@ -1,15 +1,97 @@
-
+#include "kernel_socket.h"
 #include "tinyos.h"
+#include "kernel_pipe.h"
+#include "kernel_dev.h"
+#include "kernel_streams.h"
+#include "kernel_cc.h"
+#include "kernel_sched.h"
+#include "stddef.h" 
+#include "stdio.h"
 
+
+static file_ops socket_file_ops = {
+	.Open = NULL,
+	.Read = socket_read,
+	.Write = socket_write,
+	.Close = socket_close
+};
+
+int socket_read()
+{
+
+}
+
+int socket_write()
+{
+
+}
+
+int socket_close()
+{
+
+}
 
 Fid_t sys_Socket(port_t port)
 {
-	return NOFILE;
+	if (port < 0 || port > MAX_FILEID-1){
+		return NOFILE; // Not legal port
+	}
+
+	Fid_t fid[1];
+	FCB *fcb[1];
+	if (FCB_reserve(1, fid, &fcb) == 0)	// Reserve a FCB for the socket
+	{
+		return NOFILE;
+	}
+	
+	SCB *scb = (SCB *)xmalloc(sizeof(SCB));	// Allocate memory for the socket control block
+	scb->refcount = 1;	// Set the reference count to 1
+	scb->fcb = fcb[0];	// Set the file control block
+	scb->type = SOCKET_UNBOUND;	// Set the socket type to unbound
+	scb->port = port;	// Set the port number
+	scb->socket_union.unbound_s = (unbound_socket*)xmalloc(sizeof(unbound_socket));	// Allocate memory for the unbound socket
+	rlnode_init(&(scb->socket_union.unbound_s->unbound_socket), NULL);	// Initialize the queue
+	fcb[0]->streamobj = scb;	// Set the stream object
+	fcb[0]->streamfunc = &socket_file_ops;	// Set the stream function
+
+	return fid[0];
 }
 
 int sys_Listen(Fid_t sock)
 {
-	return -1;
+	if (sock < 0 || sock > MAX_FILEID-1){
+		return -1; // Not legal socket
+	}
+
+	FCB *fcb = get_fcb(sock); // Get the FCB from the socket
+
+	// Necessary checks
+	if (fcb == NULL) {
+		return -1; // Invalid fcb
+	}
+	SCB* scb = fcb->streamobj;
+	if (scb == NULL) {
+		return -1; // Invalid socket
+	}
+	if (scb->port == NOPORT) {
+		return -1; // Socket is not bound to a port
+	}
+	
+	if (scb->type != SOCKET_UNBOUND) {
+		return -1; // Socket is not unbound
+	}
+
+	// Install the socket to the PORT_MAP[]
+	//PORT_MAP[scb->port] = fcb->streamobj;
+
+	// Mark the socket as SOCKET_LISTENER
+	scb->type = SOCKET_LISTENER;
+
+	// Initialize the listener_socket fields of the union
+	scb->socket_union.listener_s = (listener_socket*)xmalloc(sizeof(listener_socket));
+	rlnode_init(&(scb->socket_union.listener_s->queue), NULL);
+
+	return 0;
 }
 
 
---
+++ kernel_socket.h
@@ -0,0 +1,79 @@
+#ifndef __KERNEL_SOCKET_H__
+#define __KERNEL_SOCKET_H__
+
+#include "tinyos.h"
+#include "kernel_streams.h"
+#include "kernel_pipe.h"
+
+typedef struct  socket_control_block SCB;
+typedef struct listener_socket listener_socket;
+typedef struct unbound_socket unbound_socket;
+typedef struct peer_socket peer_socket;
+typedef struct connection_request connection_request;
+
+
+/**
+ * @brief Enumeration representing different types of sockets.
+ * 
+ * This enumeration defines three types of sockets:
+ * - LISTENER_SOCKET: A socket that listens for incoming connections.
+ * - UNBOUND_SOCKET: A socket that is not bound to any specific address or port.
+ * - PEER_SOCKET: A socket that is connected to a specific peer.
+ */
+typedef enum {
+    SOCKET_LISTENER,    /**< A socket that listens for incoming connections. */
+    SOCKET_UNBOUND,     /**< A socket that is not bound to any specific address or port. */
+    SOCKET_PEER         /**< A socket that is connected to a specific peer.  */
+} socket_type;
+
+
+typedef struct listener_socket {
+    rlnode queue;
+    CondVar req_available;
+} listener_socket;
+
+typedef struct unbound_socket { 
+    rlnode unbound_socket;
+} unbound_socket;
+
+typedef struct peer_socket {
+    SCB* peer;
+    PIPE_CB* write_pipe;
+    PIPE_CB* read_pipe;
+} peer_socket;
+
+/**
+ * @brief Structure representing a socket control block.
+ * 
+ * The socket control block (SCB) is used to manage the state and properties of a socket.
+ * It contains information such as the reference count, file control block (FCB), socket type,
+ * port number, and a union of different socket types (listener, unbound, peer).
+ */
+typedef struct socket_control_block {
+    uint refcount;              /**< Reference count of the socket. */
+    FCB* fcb;                   /**< File control block associated with the socket. */
+    socket_type type;           /**< Type of the socket. */
+    port_t port;                /**< Port number associated with the socket. */
+    union {
+        listener_socket* listener_s;  /**< Listener socket type. */
+        unbound_socket* unbound_s;    /**< Unbound socket type. */
+        peer_socket* peer_s;          /**< Peer socket type. */
+    } socket_union;
+} SCB;
+
+/**
+ * @brief Structure representing a connection request.
+ * 
+ * This structure is used to represent a connection request in the system.
+ * It contains information about whether the request has been admitted,
+ * the peer SCB (Socket Control Block), the connected condition variable,
+ * and the queue node for maintaining the request in a queue.
+ */
+typedef struct connection_request {
+    int admitted;           /**< Flag indicating if the request has been admitted */
+    SCB* peer;              /**< Pointer to the peer Socket Control Block */
+    CondVar connected_cv;   /**< Condition variable for signaling connection status */
+    rlnode queue_node;      /**< Queue node for maintaining the request in a queue */
+} connection_request;
+
+#endif // __KERNEL_SOCKET_H__
